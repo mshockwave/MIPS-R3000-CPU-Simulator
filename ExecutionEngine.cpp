711f5eeefb99d356e88ac8ebbac4469c8e65c2e9
mshockwave <yihshyng223@gmail.com>
2016-03-17 16:46:43 +0800

    Make ExecutionEngine non-static

--- ExecutionEngine.cpp
+++ ExecutionEngine.cpp
@@ -1,1 +1,23 @@
 #include "ExecutionEngine.h"
+
+void ExecutionEngine::DispatchTask(Instruction *instruction){
+    //Extract op code
+    uint32_t op = extractInstrBits(instruction->getBitsInstruction(), 31, 26);
+    try{
+        task_id_t taskId = task::FirstInstrOpMap.at(static_cast<uint8_t>(op));
+        DispatchTask(instruction, taskId);
+    }catch(...){
+        //TODO: Error handling: op not found
+    }
+}
+
+void ExecutionEngine::DispatchTask(Instruction* instruction, task_id_t taskId){
+    if(taskId < (task::TASK_COUNT - 1/*excluding TASK_END*/)){
+        task_id_t nextId = (task::TasksTable[taskId])(mContext, instruction);
+        /*
+         * Compiler's tail call optimization would eliminate this
+         * (Probably...)
+         * */
+        DispatchTask(instruction, nextId);
+    }
+}
--- ExecutionEngine.h
+++ ExecutionEngine.h
@@ -7,40 +7,28 @@
 
 class ExecutionEngine {
 private:
-    static Context* sContext;
+    Context* mContext;
 
-public:
-    static void SetContext(Context &ctx){ sContext = &ctx; }
-    static Context* GetContext(){
-        //Wrapper for mutex in future
-        return sContext;
-    }
-
-    static void Init(){
+    void init(){
         task::InitInstructionMap();
         task::InitTasks();
     }
 
-    static void DispatchTask(Instruction *instruction){
-        //Extract op code
-        uint32_t op = extractInstrBits(instruction->getBitsInstruction(), 31, 26);
-        try{
-            task_id_t taskId = task::FirstInstrOpMap.at(static_cast<uint8_t>(op));
-            DispatchTask(instruction, taskId);
-        }catch(...){
-            //TODO: Error handling: op not found
-        }
+public:
+
+    Context* GetContext(){
+        //Wrapper for mutex in future
+        return mContext;
     }
-    static void DispatchTask(Instruction* instruction, task_id_t taskId){
-        if(taskId < (task::TASK_COUNT - 1/*excluding TASK_END*/)){
-            task_id_t nextId = (task::TasksTable[taskId])(sContext, instruction);
-            /*
-             * Compiler's tail call optimization would eliminate this
-             * (Probably...)
-             * */
-            DispatchTask(instruction, nextId);
-        }
+
+    ExecutionEngine(Context& ctx) :
+            mContext(&ctx){
+        init();
     }
+
+    void DispatchTask(Instruction *instruction);
+
+    void DispatchTask(Instruction* instruction, task_id_t taskId);
 };
 
 #endif //ARCHIHW1_EXECUTIONENGINE_H
