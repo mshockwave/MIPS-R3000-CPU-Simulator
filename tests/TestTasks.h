9436003781f9097eed71b9fe8ff9609655a15595
mshockwave <yihshyng223@gmail.com>
2016-04-28 08:45:58 +0800

    Separate printer unit test cases from value unit test cases

--- tests/TestTasks.h
+++ tests/TestTasks.h
@@ -85,10 +85,256 @@ private:
 #endif
     }
 
-    bool testMultiThreadNoStall(){
+    bool verifyValMultiThreadNoStall(){
 #ifndef NDEBUG
 
-        Log::D(mName) << "Testing two tasks with no stalling..." << std::endl;
+        Log::D(mName) << "Verifying value: two tasks with no stalling..." << std::endl;
+
+        Context ctx(std::cout, std::cout);
+        task::InitInstructionMap();
+        task::InitTasks();
+
+        ctx.mInstrStartAddr = 0x00;
+        ctx.Registers[1] = 0xA;
+        ctx.Registers[2] = 0xC;
+
+        Instructions instructions;
+        /* add $4, $1, $2 */
+        const byte_t raw_add_instr[4] = { 0x00, 0x22, 0x20, 0x20 };
+        Instruction add_instr(raw_add_instr);
+        instructions.mInstructions.push_back(add_instr);
+        /* sub $3, $2, $1 */
+        const byte_t raw_sub_instr[4] = { 0x00, 0x41, 0x18, 0x22 };
+        Instruction sub_instr(raw_sub_instr);
+        instructions.mInstructions.push_back(sub_instr);
+        /* halt */
+        const byte_t raw_halt_instr[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+        Instruction halt_instr(raw_halt_instr);
+        instructions.mInstructions.push_back(halt_instr);
+
+        ctx.setInstructionCount((uint32_t)instructions.length());
+
+        TaskHandle::ClockHandle::Barrier rising_barrier(5);
+        TaskHandle::ClockHandle::Barrier falling_barrier(5);
+        TaskHandle::ClockHandle clock_handle(rising_barrier, falling_barrier);
+
+        boost::thread_group group;
+        /*IF*/
+        group.create_thread([&]()->void{
+            IFEngine engine(ctx, instructions, clock_handle);
+            engine.Start();
+        });
+        /*ID*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::IDEngineRunnable);
+            engine.Start();
+        });
+        /*EX*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::EXEngineRunnable);
+            engine.Start();
+        });
+        /*DM*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::DMEngineRunnable);
+            engine.Start();
+        });
+        /*WB*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::WBEngineRunnable);
+            engine.Start();
+        });
+
+        group.join_all();
+
+        AssertEqual((int)ctx.Registers[4], 22, "$4 Value");
+        AssertEqual((int)ctx.Registers[3], 2, "$3 Value");
+
+        return true;
+#else
+        Log::E(mName) << "Need to test under Debug configuration" << std::endl;
+        return false;
+#endif
+    }
+
+    /*
+     * Two instructions
+     * One inserted NOP
+     * */
+    bool verifyValMultiThreadStall1(){
+#ifndef NDEBUG
+
+        Log::D(mName) << "Verifying value: two tasks with stalling..." << std::endl;
+
+        Context ctx(std::cout, std::cout);
+        task::InitInstructionMap();
+        task::InitTasks();
+
+        ctx.mInstrStartAddr = 0x00;
+        ctx.Registers[1] = 0xA;
+        ctx.Registers[2] = 0xC;
+
+        Instructions instructions;
+        /* add $4, $1, $2 */
+        const byte_t raw_add_instr[4] = { 0x00, 0x22, 0x20, 0x20 };
+        Instruction add_instr(raw_add_instr);
+        instructions.mInstructions.push_back(add_instr);
+        /* sub $3, $4, $1 */
+        const byte_t raw_sub_instr[4] = { 0x00, 0x81, 0x18, 0x22 };
+        Instruction sub_instr(raw_sub_instr);
+        instructions.mInstructions.push_back(sub_instr);
+        /* halt */
+        const byte_t raw_halt_instr[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+        Instruction halt_instr(raw_halt_instr);
+        instructions.mInstructions.push_back(halt_instr);
+
+        ctx.setInstructionCount((uint32_t)instructions.length());
+
+        TaskHandle::ClockHandle::Barrier rising_barrier(5);
+        TaskHandle::ClockHandle::Barrier falling_barrier(5);
+        TaskHandle::ClockHandle clock_handle(rising_barrier, falling_barrier);
+
+        boost::thread_group group;
+        /*IF*/
+        group.create_thread([&]()->void{
+            IFEngine engine(ctx, instructions, clock_handle);
+            engine.Start();
+        });
+        /*ID*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::IDEngineRunnable);
+            engine.Start();
+        });
+        /*EX*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::EXEngineRunnable);
+            engine.Start();
+        });
+        /*DM*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::DMEngineRunnable);
+            engine.Start();
+        });
+        /*WB*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::WBEngineRunnable);
+            engine.Start();
+        });
+
+        group.join_all();
+
+        //Log::V(mName) << "$4 Value: " << (int)ctx.Registers[4] << std::endl;
+        //Log::V(mName) << "$3 Value: " << (int)ctx.Registers[3] << std::endl;
+        AssertEqual((int)ctx.Registers[4], 22, "$4 Value");
+        AssertEqual((int)ctx.Registers[3], 0xC, "$3 Value");
+
+        return true;
+#else
+        Log::E(mName) << "Need to test under Debug configuration" << std::endl;
+        return false;
+#endif
+    }
+
+    /*
+     * Three instructions
+     * One inserted NOP
+     * */
+    bool verifyValMultiThreadStall2(){
+#ifndef NDEBUG
+
+        Log::D(mName) << "Verifying value: three tasks with stalling..." << std::endl;
+
+        Context ctx(std::cout, std::cout);
+        task::InitInstructionMap();
+        task::InitTasks();
+
+        ctx.mInstrStartAddr = 0x00;
+        ctx.Registers[1] = 0xA;
+        ctx.Registers[2] = 0xC;
+
+        Instructions instructions;
+        /* add $4, $1, $2 */
+        const byte_t raw_add_instr[4] = { 0x00, 0x22, 0x20, 0x20 };
+        Instruction add_instr(raw_add_instr);
+        instructions.mInstructions.push_back(add_instr);
+        /* sub $3, $4, $1 */
+        const byte_t raw_sub_instr[4] = { 0x00, 0x81, 0x18, 0x22 };
+        Instruction sub_instr(raw_sub_instr);
+        instructions.mInstructions.push_back(sub_instr);
+        /* add $5, $1, $2 */
+        const byte_t raw_add_instr2[4] = { 0x00, 0x22, 0x28, 0x20 };
+        Instruction add_instr2(raw_add_instr2);
+        instructions.mInstructions.push_back(add_instr2);
+        /* halt */
+        const byte_t raw_halt_instr[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+        Instruction halt_instr(raw_halt_instr);
+        instructions.mInstructions.push_back(halt_instr);
+
+        ctx.setInstructionCount((uint32_t)instructions.length());
+
+        TaskHandle::ClockHandle::Barrier rising_barrier(5);
+        TaskHandle::ClockHandle::Barrier falling_barrier(5);
+        TaskHandle::ClockHandle clock_handle(rising_barrier, falling_barrier);
+
+        boost::thread_group group;
+        /*IF*/
+        group.create_thread([&]()->void{
+            IFEngine engine(ctx, instructions, clock_handle);
+            engine.Start();
+        });
+        /*ID*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::IDEngineRunnable);
+            engine.Start();
+        });
+        /*EX*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::EXEngineRunnable);
+            engine.Start();
+        });
+        /*DM*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::DMEngineRunnable);
+            engine.Start();
+        });
+        /*WB*/
+        group.create_thread([&]()->void{
+            ExecutionEngine engine(ctx, clock_handle,
+                                   engines::WBEngineRunnable);
+            engine.Start();
+        });
+
+        group.join_all();
+
+        //Log::V(mName) << "$4 Value: " << (int)ctx.Registers[4] << std::endl;
+        //Log::V(mName) << "$3 Value: " << (int)ctx.Registers[3] << std::endl;
+        //Log::V(mName) << "$5 Value: " << (int)ctx.Registers[5] << std::endl;
+        AssertEqual((int)ctx.Registers[4], 22, "$4 Value");
+        AssertEqual((int)ctx.Registers[3], 0xC, "$3 Value");
+        AssertEqual((int)ctx.Registers[5], 22, "$5 Value");
+
+        return true;
+#else
+        Log::E(mName) << "Need to test under Debug configuration" << std::endl;
+        return false;
+#endif
+    }
+
+    bool verifyPrinterMultiThreadNoStall(){
+#ifndef NDEBUG
+
+        Log::D(mName) << "Verifying printer: two tasks with no stalling..." << std::endl;
 
         Context ctx(std::cout, std::cout);
         task::InitInstructionMap();
@@ -149,7 +395,6 @@ private:
             engine.Start();
         });
 
-        //group.join_all();
         ctx.StartPrinterLoop(if_thread,
                              id_thread,
                              ex_thread,
@@ -165,102 +410,14 @@ private:
         return false;
 #endif
     }
-
-    /*
-     * Two instructions
-     * One inserted NOP
-     * */
-    bool testMultiThreadStall1(){
-#ifndef NDEBUG
-
-        Log::D(mName) << "Testing two tasks with stalling..." << std::endl;
-
-        Context ctx(std::cout, std::cout);
-        task::InitInstructionMap();
-        task::InitTasks();
-
-        ctx.mInstrStartAddr = 0x00;
-        ctx.Registers[1] = 0xA;
-        ctx.Registers[2] = 0xC;
-
-        Instructions instructions;
-        /* add $4, $1, $2 */
-        const byte_t raw_add_instr[4] = { 0x00, 0x22, 0x20, 0x20 };
-        Instruction add_instr(raw_add_instr);
-        instructions.mInstructions.push_back(add_instr);
-        /* sub $3, $4, $1 */
-        const byte_t raw_sub_instr[4] = { 0x00, 0x81, 0x18, 0x22 };
-        Instruction sub_instr(raw_sub_instr);
-        instructions.mInstructions.push_back(sub_instr);
-        /* halt */
-        const byte_t raw_halt_instr[4] = {0xFF, 0xFF, 0xFF, 0xFF};
-        Instruction halt_instr(raw_halt_instr);
-        instructions.mInstructions.push_back(halt_instr);
-
-        ctx.setInstructionCount((uint32_t)instructions.length());
-
-        TaskHandle::ClockHandle::Barrier rising_barrier(5);
-        TaskHandle::ClockHandle::Barrier falling_barrier(5);
-        TaskHandle::ClockHandle clock_handle(rising_barrier, falling_barrier);
-
-        boost::thread_group group;
-        /*IF*/
-        auto* if_thread = group.create_thread([&]()->void{
-            IFEngine engine(ctx, instructions, clock_handle);
-            engine.Start();
-        });
-        /*ID*/
-        auto* id_thread = group.create_thread([&]()->void{
-            ExecutionEngine engine(ctx, clock_handle,
-                                   engines::IDEngineRunnable);
-            engine.Start();
-        });
-        /*EX*/
-        auto* ex_thread = group.create_thread([&]()->void{
-            ExecutionEngine engine(ctx, clock_handle,
-                                   engines::EXEngineRunnable);
-            engine.Start();
-        });
-        /*DM*/
-        auto* dm_thread = group.create_thread([&]()->void{
-            ExecutionEngine engine(ctx, clock_handle,
-                                   engines::DMEngineRunnable);
-            engine.Start();
-        });
-        /*WB*/
-        auto* wb_thread = group.create_thread([&]()->void{
-            ExecutionEngine engine(ctx, clock_handle,
-                                   engines::WBEngineRunnable);
-            engine.Start();
-        });
-
-        //group.join_all();
-        ctx.StartPrinterLoop(if_thread,
-                             id_thread,
-                             ex_thread,
-                             dm_thread,
-                             wb_thread);
-
-        //Log::V(mName) << "$4 Value: " << (int)ctx.Registers[4] << std::endl;
-        //Log::V(mName) << "$3 Value: " << (int)ctx.Registers[3] << std::endl;
-        AssertEqual((int)ctx.Registers[4], 22, "$4 Value");
-        AssertEqual((int)ctx.Registers[3], 0xC, "$3 Value");
-
-        return true;
-#else
-        Log::E(mName) << "Need to test under Debug configuration" << std::endl;
-        return false;
-#endif
-    }
-
     /*
      * Three instructions
      * One inserted NOP
      * */
-    bool testMultiThreadStall2(){
+    bool verifyPrinterMultiThreadStall(){
 #ifndef NDEBUG
 
-        Log::D(mName) << "Testing three tasks with stalling..." << std::endl;
+        Log::D(mName) << "Verifying printer: three tasks with stalling..." << std::endl;
 
         Context ctx(std::cout, std::cout);
         task::InitInstructionMap();
@@ -325,7 +482,6 @@ private:
             engine.Start();
         });
 
-        //group.join_all();
         ctx.StartPrinterLoop(if_thread,
                              id_thread,
                              ex_thread,
@@ -351,9 +507,11 @@ private:
         bool result = true;
 
         result &= testSingleThread();
-        result &= testMultiThreadNoStall();
-        result &= testMultiThreadStall1();
-        result &= testMultiThreadStall2();
+        result &= verifyValMultiThreadNoStall();
+        result &= verifyValMultiThreadStall1();
+        result &= verifyValMultiThreadStall2();
+        result &= verifyPrinterMultiThreadNoStall();
+        result &= verifyPrinterMultiThreadStall();
 
         return result;
     }
