f4689222112e331b7d8c386f9f2bd8a4145e7b74
mshockwave <yihshyng223@gmail.com>
2016-04-30 22:06:12 +0800

    Add J type instructions(not tested)

--- tasks/JTasks.cpp
+++ tasks/JTasks.cpp
@@ -1,7 +1,87 @@
 #include "JTasks.h"
 
 namespace task{
+    
+    namespace JInstr{
+        
+        TaskHandle::stage_task_t EmptyIF = STAGE_TASK(){
+            auto* ctx = self->context;
+            
+            RISING_EDGE_FENCE();
+            
+            return (ctx->pushTask(ctx->IF_ID, self))? Error::NONE : Error::PIPELINE_STALL;
+        };
+        
+        TaskHandle::stage_task_t EmptyEX = STAGE_TASK(){
+            auto* ctx = self->context;
+            
+            RISING_EDGE_FENCE();
+            
+            return (ctx->pushTask(ctx->EX_DM, self))? Error::NONE : Error::PIPELINE_STALL;
+        };
+        
+        TaskHandle::stage_task_t EmptyDM = STAGE_TASK(){
+            auto* ctx = self->context;
+            
+            RISING_EDGE_FENCE();
+            
+            return (ctx->pushTask(ctx->DM_WB, self))? Error::NONE : Error::PIPELINE_STALL;
+        };
+        
+        TaskHandle::stage_task_t EmptyWB = STAGE_TASK(){
+            
+            RISING_EDGE_FENCE();
+            
+            return Error::NONE;
+        };
+        
+    }; //namespace JInstr
+    
     void InitJTasks(){
-
+        
+        TasksTable[OP_J].Name("J", OP_J)
+        .IF(JInstr::EmptyIF)
+        .ID(STAGE_TASK(){
+            auto* ctx = self->context;
+            
+            RISING_EDGE_FENCE();
+            
+            uint32_t addr = JInstr::GetAddr(self->instruction->GetBitsInstruction());
+            reg_t pc = ctx->GetPC();
+            pc += WORD_WIDTH;
+            uint32_t partPC = extractInstrBits(pc, 31, 28);
+            pc = (partPC << 28);
+            
+            Error e = ctx->SetPC( pc | (addr << 2) );
+            
+            return e;
+        })
+        .EX(JInstr::EmptyEX)
+        .DM(JInstr::EmptyDM)
+        .WB(JInstr::EmptyWB);
+        
+        TasksTable[OP_JAL].Name("JAL", OP_JAL)
+        .IF(JInstr::EmptyIF)
+        .ID(STAGE_TASK(){
+            auto* ctx = self->context;
+            
+            RISING_EDGE_FENCE();
+            
+            uint32_t addr = JInstr::GetAddr(self->instruction->GetBitsInstruction());
+            reg_t pc = ctx->GetPC();
+            pc += WORD_WIDTH;
+            ctx->RA = pc;
+            
+            uint32_t partPC = extractInstrBits(pc, 31, 28);
+            pc = (partPC << 28);
+            
+            Error e = ctx->SetPC( pc | (addr << 2) );
+            
+            return e;
+        })
+        .EX(JInstr::EmptyEX)
+        .DM(JInstr::EmptyDM)
+        .WB(JInstr::EmptyWB);
+        
     }
 }
