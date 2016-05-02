80e0f8bd38d06a901461e3d267bf225a8e91fedc
mshockwave <yihshyng223@gmail.com>
2016-05-02 08:53:14 +0800

    Fixing JAL behavior

--- tasks/JTasks.cpp
+++ tasks/JTasks.cpp
@@ -71,7 +71,9 @@ namespace task{
             uint32_t addr = JInstr::GetAddr(self->instruction->GetBitsInstruction());
             reg_t pc = ctx->GetPC();
             pc += WORD_WIDTH;
-            ctx->RA = pc;
+            //Store $ra value in self->Rd
+            self->RdValue = pc;
+            self->RdIndex = 31; //$ra
             
             uint32_t partPC = extractInstrBits(pc, 31, 28);
             pc = (partPC << 28);
@@ -79,11 +81,41 @@ namespace task{
             ctx->PcFlush = Context::PC_FLUSH_CONSUMER_COUNT;
             Error e = ctx->SetPC( pc | (addr << 2) );
             
+            //Reserve $ra
+            ctx->RegReserves[self->RdIndex].Reset(self);
+            
             return (ctx->pushTask(ctx->ID_EX, self))? e : Error::PIPELINE_STALL;
         })
-        .EX(JInstr::EmptyEX)
+        .EX(STAGE_TASK(){
+            auto* ctx = self->context;
+            auto& reg_reserves = ctx->RegReserves;
+            
+            reg_reserves[self->RdIndex].EXAvailable = true;
+            
+            RISING_EDGE_FENCE();
+            
+            //Do the forwarding job
+            reg_reserves[self->RdIndex].Value = self->RdValue;
+            reg_reserves[self->RdIndex].IDAvailable = true;
+            
+            return (ctx->pushTask(ctx->EX_DM, self))? Error::NONE : Error::PIPELINE_STALL;
+        })
         .DM(JInstr::EmptyDM)
-        .WB(JInstr::EmptyWB);
+        .WB(STAGE_TASK(){
+            auto* ctx = self->context;
+            
+            ctx->Registers[self->RdIndex] = self->RdValue;
+            self->ModifyRegIndex = self->RdIndex;
+            
+            //Clean destination register reservation
+            if(ctx->RegReserves[self->RdIndex].Holder == self){
+                ctx->RegReserves[self->RdIndex].Reset(nullptr);
+            }
+            
+            RISING_EDGE_FENCE();
+            
+            return Error::NONE;
+        });
         
     }
 }
